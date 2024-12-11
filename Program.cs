3bdb50d44b5c1bb8cb1977ddf8a20ed3794821ea
AdliXSec <naufalsyahruradli@gmail.com>
2024-12-11 09:50:38 +0700

    Tubes Console App Keuangan

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+
+string host, user, password, database;
+
+host = "localhost";
+user = "root";
+password = "";
+database = "tubes_keuangan";
+
+string connectionString = $"server={host};user={user};password={password};database={database};";
+
+MySqlConnection connection = new MySqlConnection(connectionString);
+
+try
+{
+    connection.Open();
+    // Console.WriteLine("Koneksi berhasil!");
+
+    TampilData(connection);
+    connection.Close();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Terdapat error: {ex.Message}");
+}
+
+static void TampilData(MySqlConnection connection)
+{
+    Console.WriteLine("Data Keuangan");
+    Console.WriteLine("==============");
+
+    string query = "SELECT * FROM user";
+    MySqlCommand command = new MySqlCommand(query, connection);
+    MySqlDataReader reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string nama = reader.GetString("name_user");
+        int saldo = reader.GetInt32("saldo_user");
+
+        Console.WriteLine($@"
+        Nama: {nama}           Saldo: {saldo}
+        ");
+    }
+}
