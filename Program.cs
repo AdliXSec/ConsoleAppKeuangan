b2e557b87fc500ac7a7894cc60641eab39742644
AdliXSec <naufalsyahruradli@gmail.com>
2024-12-12 01:31:56 +0700

    change db to excel

--- Program.cs
+++ Program.cs
@@ -1,46 +1,132 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
 
-string host, user, password, database;
+string filePath = @"database/database.xlsx";
+string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\"";
 
-host = "localhost";
-user = "root";
-password = "";
-database = "tubes_keuangan";
-
-string connectionString = $"server={host};user={user};password={password};database={database};";
-
-MySqlConnection connection = new MySqlConnection(connectionString);
+int menu = 0;
 
 try
 {
-    connection.Open();
-    // Console.WriteLine("Koneksi berhasil!");
-
-    TampilData(connection);
-    connection.Close();
-
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Terdapat error: {ex.Message}");
-}
-
-static void TampilData(MySqlConnection connection)
-{
-    Console.WriteLine("Data Keuangan");
-    Console.WriteLine("==============");
-
-    string query = "SELECT * FROM user";
-    MySqlCommand command = new MySqlCommand(query, connection);
-    MySqlDataReader reader = command.ExecuteReader();
-
-    while (reader.Read())
+    while (menu != 5)
     {
-        string nama = reader.GetString("name_user");
-        int saldo = reader.GetInt32("saldo_user");
-
-        Console.WriteLine($@"
-        Nama: {nama}           Saldo: {saldo}
-        ");
+        ClearScreen();
+        Console.WriteLine(@"Wellcome To Mangan Bata C 
+(Manajemen Keuangan Berbasis Database dan C#)");
+        ReadUser(connectionString);
+        Menu(menu, connectionString);
     }
+    // ReadData(connectionString);
 }
+catch (Exception e)
+{
+    Console.WriteLine($"terjadi kesalahan {e}");
+}
+
+// bersihkan layar
+
+static void ClearScreen()
+{
+    Console.Clear();
+}
+
+// tampilkan menu
+
+static void Menu(int menu, string connectionString)
+{
+    Console.Write(@"
+1. Riwayat Transaksi
+2. Tambah Transaksi
+3. Hapus Transaksi
+4. Edit User
+5. Keluar
+
+Pilih Menu : ");
+    menu = Convert.ToInt32(Console.ReadLine());
+
+    switch (menu)
+    {
+        case 1:
+            ReadTransaksi(connectionString);
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            return;
+        default:
+            Console.WriteLine("Pilihan tidak valid");
+            break;
+    }
+
+}
+
+// baca data user
+
+static void ReadUser(string connectionString)
+{
+    string query = "SELECT * FROM [user$]";
+#pragma warning disable
+    using (OleDbConnection connection = new OleDbConnection(connectionString))
+    {
+        connection.Open();
+        using (OleDbCommand command = new OleDbCommand(query, connection))
+        using (OleDbDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine(@$"
+    {reader["NAME_KEUANGAN"]}
+    Rp. {reader["SALDO_KEUANGAN"]}");
+            }
+        }
+    }
+#pragma warning restore
+}
+
+// baca riwayat transakssi
+
+static void ReadTransaksi(string connectionString)
+{
+    string query2 = "SELECT * FROM [transaksi$]";
+#pragma warning disable CA1416 // Validate platform compatibility
+
+    using (OleDbConnection connection = new OleDbConnection(connectionString))
+    {
+        connection.Open();
+        using (OleDbCommand command2 = new OleDbCommand(query2, connection))
+        using (OleDbDataReader reader2 = command2.ExecuteReader())
+        {
+            Console.WriteLine(@"
+==== RIWAYAT TRANSAKSI ====
+                ");
+            while (reader2.Read())
+            {
+                ClearScreen();
+                Console.WriteLine(@$"
+Jenis : {reader2["JENIS_TRANSAKSI"]}
+Total: {reader2["SALDO_TRANSAKSI"]}
+
+{reader2["KETERANGAN_TRANSAKSI"]}
+
+{reader2["TANGGAL_TRANSAKSI"]}");
+            }
+            Console.WriteLine(@"
+===========================
+                ");
+        }
+    }
+#pragma warning restore CA1416 // Validate platform compatibility
+}
+
+// static void ExecuteNonQuery(string query, string connectionString)
+// {
+// #pragma warning disable
+//     using (OleDbConnection connection = new OleDbConnection(connectionString))
+//     {
+//         connection.Open();
+//         using (OleDbCommand command = new OleDbCommand(query, connection))
+//         {
+//             command.ExecuteNonQuery();
+//         }
+//     }
+// #pragma warning restore
+// }
